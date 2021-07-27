ba0951fbe8014747b3833ecde4d615ab9c739a47
scalde44 <scalderon1504@gmail.com>
2021-07-27 15:03:00 -0500

    Definicion metodos de crear, eliminar y actualizar

---
+++ microservicio/dominio/src/main/java/com/ceiba/mantenimiento/puerto/repositorio/RepositorioMantenimiento.java
@@ -0,0 +1,43 @@
+package com.ceiba.mantenimiento.puerto.repositorio;
+
+import com.ceiba.mantenimiento.modelo.entidad.Mantenimiento;
+
+public interface RepositorioMantenimiento {
+	/**
+	 * Permite crear un mantenimiento
+	 * 
+	 * @param mantenimiento
+	 * @return el id generado
+	 */
+	Long crear(Mantenimiento mantenimiento);
+
+	/**
+	 * Permite actualizar un mantenimiento
+	 * 
+	 * @param usuario
+	 */
+	void actualizar(Mantenimiento mantenimiento);
+
+	/**
+	 * Permite eliminar un mantenimiento
+	 * 
+	 * @param id
+	 */
+	void eliminar(Long id);
+
+	/**
+	 * Permite validar si existe un mantenimiento con esa placa activo
+	 * 
+	 * @param placa
+	 * @return si existe o no
+	 */
+	boolean existeActivo(String placa);
+
+	/**
+	 * Permite validar si existe un mantenimiento con una placa incluyendo un id
+	 * 
+	 * @param id, placa
+	 * @return si existe o no
+	 */
+	boolean existeIncluyendoId(Long id, String placa);
+}
