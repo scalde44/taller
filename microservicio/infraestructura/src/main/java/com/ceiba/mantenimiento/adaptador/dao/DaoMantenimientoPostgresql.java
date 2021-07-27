53e422010c5da51a318da6fcbf3fa054b6b30b68
scalde44 <scalderon1504@gmail.com>
2021-07-27 15:06:04 -0500

    Implementacion de los metodos dao

---
+++ microservicio/infraestructura/src/main/java/com/ceiba/mantenimiento/adaptador/dao/DaoMantenimientoPostgresql.java
@@ -0,0 +1,60 @@
+package com.ceiba.mantenimiento.adaptador.dao;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.stereotype.Component;
+
+import com.ceiba.infraestructura.jdbc.CustomNamedParameterJdbcTemplate;
+import com.ceiba.infraestructura.jdbc.sqlstatement.SqlStatement;
+import com.ceiba.mantenimiento.modelo.dto.DtoMantenimiento;
+import com.ceiba.mantenimiento.puerto.dao.DaoMantenimiento;
+
+@Component
+public class DaoMantenimientoPostgresql implements DaoMantenimiento {
+	private final CustomNamedParameterJdbcTemplate customNamedParameterJdbcTemplate;
+
+	@SqlStatement(namespace = "mantenimiento", value = "listar")
+	private static String sqlListar;
+	@SqlStatement(namespace = "mantenimiento", value = "listarActivos")
+	private static String sqlListarActivos;
+	@SqlStatement(namespace = "mantenimiento", value = "buscarPorId")
+	private static String sqlBuscarPorId;
+	@SqlStatement(namespace = "mantenimiento", value = "listarActivosPorFecha")
+	private static String sqlListarActivosPorFecha;
+
+	public DaoMantenimientoPostgresql(CustomNamedParameterJdbcTemplate customNamedParameterJdbcTemplate) {
+		this.customNamedParameterJdbcTemplate = customNamedParameterJdbcTemplate;
+	}
+
+	@Override
+	public List<DtoMantenimiento> listar() {
+		return this.customNamedParameterJdbcTemplate.getNamedParameterJdbcTemplate().query(sqlListar,
+				new MapeoMantenimiento());
+	}
+
+	@Override
+	public List<DtoMantenimiento> listarActivos() {
+		return this.customNamedParameterJdbcTemplate.getNamedParameterJdbcTemplate().query(sqlListarActivos,
+				new MapeoMantenimiento());
+	}
+
+	@Override
+	public List<DtoMantenimiento> listarActivosPorFecha(String fecha) {
+		MapSqlParameterSource paramSource = new MapSqlParameterSource();
+		paramSource.addValue("fecha", fecha);
+		return this.customNamedParameterJdbcTemplate.getNamedParameterJdbcTemplate().query(sqlListarActivosPorFecha,
+				paramSource, new MapeoMantenimiento());
+	}
+
+	@Override
+	public Optional<DtoMantenimiento> buscarPorId(Long id) {
+		MapSqlParameterSource paramSource = new MapSqlParameterSource();
+		paramSource.addValue("id", id);
+
+		return this.customNamedParameterJdbcTemplate.getNamedParameterJdbcTemplate()
+				.query(sqlBuscarPorId, paramSource, new MapeoMantenimiento()).stream().findFirst();
+	}
+
+}
