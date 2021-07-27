08c933f31a80195d96d71206ca5df6b5627018d6
scalde44 <scalderon1504@gmail.com>
2021-07-27 15:06:24 -0500

    RowMapper de mantenimiento

---
+++ microservicio/infraestructura/src/main/java/com/ceiba/mantenimiento/adaptador/dao/MapeoMantenimiento.java
@@ -0,0 +1,26 @@
+package com.ceiba.mantenimiento.adaptador.dao;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.LocalDateTime;
+
+import org.springframework.jdbc.core.RowMapper;
+
+import com.ceiba.infraestructura.jdbc.MapperResult;
+import com.ceiba.mantenimiento.modelo.dto.DtoMantenimiento;
+
+public class MapeoMantenimiento implements RowMapper<DtoMantenimiento>, MapperResult {
+
+	@Override
+	public DtoMantenimiento mapRow(ResultSet resultSet, int rowNum) throws SQLException {
+		Long id = resultSet.getLong("id");
+		String placa = resultSet.getString("placa");
+		Integer cilindraje = resultSet.getInt("cilindraje");
+		LocalDateTime fechaEntrada = extraerLocalDateTime(resultSet, "fecha_entrada");
+		Integer tarifa = resultSet.getInt("tarifa");
+		String estado = resultSet.getString("estado");
+
+		return new DtoMantenimiento(id, placa, cilindraje, fechaEntrada, tarifa, estado);
+	}
+
+}
