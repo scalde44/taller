3020e5a19967f033d8eece570685c771c4b4bfa3
scalde44 <scalderon1504@gmail.com>
2021-07-28 17:18:30 -0500

    Se agrega el dirtiescontext

--- microservicio/infraestructura/src/test/java/com/ceiba/mantenimiento/controlador/ComandoControladorMantenimientoTest.java
+++ microservicio/infraestructura/src/test/java/com/ceiba/mantenimiento/controlador/ComandoControladorMantenimientoTest.java
@@ -7,10 +7,13 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import java.time.LocalDateTime;
 
 import org.junit.Test;
+import org.junit.jupiter.api.Order;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.annotation.DirtiesContext.MethodMode;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
@@ -32,8 +35,12 @@ public class ComandoControladorMantenimientoTest {
 	private ObjectMapper objectMapper;
 
 	private static final LocalDateTime FECHA_CON_HORA_VALIDA = LocalDateTime.of(2021, 8, 1, 10, 0);
+	private static final String PLACA_SEGUNDO_REGISTRO = "FNF327";
+	private static final Long ID_SEGUNDO_REGISTRO = 2L;
 
+	@DirtiesContext(methodMode = MethodMode.AFTER_METHOD)
 	@Test
+	@Order(1)
 	public void crear() throws Exception {
 		ComandoMantenimiento mantenimiento = new ComandoMantenimientoTestDataBuilder().conFecha(FECHA_CON_HORA_VALIDA)
 				.build();
@@ -43,21 +50,23 @@ public class ComandoControladorMantenimientoTest {
 				.andExpect(MockMvcResultMatchers.status().isOk()).andExpect(jsonPath("$.valor").exists());
 	}
 
+	@DirtiesContext(methodMode = MethodMode.AFTER_METHOD)
 	@Test
+	@Order(2)
 	public void actualizar() throws Exception {
-		ComandoMantenimiento mantenimiento = new ComandoMantenimientoTestDataBuilder().conFecha(FECHA_CON_HORA_VALIDA)
-				.build();
-		mocMvc.perform(MockMvcRequestBuilders.post("/mantenimientos").contentType(MediaType.APPLICATION_JSON)
-				.content(objectMapper.writeValueAsString(mantenimiento)))
-				.andExpect(MockMvcResultMatchers.status().isOk()).andExpect(jsonPath("$.valor").exists());
-		mocMvc.perform(MockMvcRequestBuilders.put("/mantenimientos/{id}", 2L).contentType(MediaType.APPLICATION_JSON)
-				.content(objectMapper.writeValueAsString(mantenimiento)))
+		ComandoMantenimiento mantenimiento = new ComandoMantenimientoTestDataBuilder().conPlaca(PLACA_SEGUNDO_REGISTRO)
+				.conFecha(FECHA_CON_HORA_VALIDA).build();
+
+		mocMvc.perform(MockMvcRequestBuilders.put("/mantenimientos/{id}", ID_SEGUNDO_REGISTRO)
+				.contentType(MediaType.APPLICATION_JSON).content(objectMapper.writeValueAsString(mantenimiento)))
 				.andExpect(MockMvcResultMatchers.status().isOk());
 	}
 
+	@DirtiesContext(methodMode = MethodMode.AFTER_METHOD)
 	@Test
+	@Order(3)
 	public void eliminar() throws Exception {
-		mocMvc.perform(delete("/mantenimientos/{id}", 2L).contentType(MediaType.APPLICATION_JSON)
+		mocMvc.perform(delete("/mantenimientos/{id}", ID_SEGUNDO_REGISTRO).contentType(MediaType.APPLICATION_JSON)
 				.accept(MediaType.APPLICATION_JSON)).andExpect(status().isOk());
 	}
 
--- microservicio/infraestructura/src/test/java/com/ceiba/mantenimiento/controlador/ConsultaControladorMantenimientoTest.java
+++ microservicio/infraestructura/src/test/java/com/ceiba/mantenimiento/controlador/ConsultaControladorMantenimientoTest.java
@@ -1,9 +1,12 @@
 package com.ceiba.mantenimiento.controlador;
 
+import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.core.Is.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.time.LocalDate;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -23,45 +26,40 @@ public class ConsultaControladorMantenimientoTest {
 	@Autowired
 	private MockMvc mocMvc;
 
-	private static final String PLACA_PRUEBA = "GCY035";
-	private static final int CILINDRAJE_PRUEBA = 250;
-	private static final String ESTADO_PRUEBA = "A";
+	private static final String PLACA_PRUEBA_INACTIVO = "FNF327";
+	private static final int CILINDRAJE_PRUEBA_INACTIVO = 150;
+	private static final String ESTADO_ACTIVO = "A";
+	private static final String ESTADO_INACTIVO = "I";
+	private static final long ID_PRUEBA_INACTIVO = 2L;
 
 	@Test
 	public void listar() throws Exception {
-		// arrange
-
-		// act - assert
 		mocMvc.perform(get("/mantenimientos").contentType(MediaType.APPLICATION_JSON)).andExpect(status().isOk())
-				.andExpect(jsonPath("$[0].id", is(1)));
+				.andExpect(jsonPath("$", hasSize(2))).andExpect(jsonPath("$[0].id", is(1)))
+				.andExpect(jsonPath("$[1].placa", is(PLACA_PRUEBA_INACTIVO)));
 	}
 
 	@Test
 	public void listarActivos() throws Exception {
-		// arrange
-
-		// act - assert
 		mocMvc.perform(get("/mantenimientos/activos").contentType(MediaType.APPLICATION_JSON))
-				.andExpect(status().isOk()).andExpect(jsonPath("$[0].estado", is(ESTADO_PRUEBA)));
+				.andExpect(status().isOk()).andExpect(jsonPath("$", hasSize(1)))
+				.andExpect(jsonPath("$[0].estado", is(ESTADO_ACTIVO)));
 	}
 
 	@Test
 	public void buscarPorId() throws Exception {
-		// arrange
-
-		// act - assert
-		mocMvc.perform(get("/mantenimientos/1").contentType(MediaType.APPLICATION_JSON)).andExpect(status().isOk())
-				.andExpect(jsonPath("$.id").exists()).andExpect(jsonPath("$.placa", is(PLACA_PRUEBA)))
-				.andExpect(jsonPath("$.cilindraje", is(CILINDRAJE_PRUEBA)))
-				.andExpect(jsonPath("$.estado", is(ESTADO_PRUEBA)));
+		mocMvc.perform(get("/mantenimientos/{id}", ID_PRUEBA_INACTIVO).contentType(MediaType.APPLICATION_JSON))
+				.andExpect(status().isOk()).andExpect(jsonPath("$.id").exists())
+				.andExpect(jsonPath("$.placa", is(PLACA_PRUEBA_INACTIVO)))
+				.andExpect(jsonPath("$.cilindraje", is(CILINDRAJE_PRUEBA_INACTIVO)))
+				.andExpect(jsonPath("$.estado", is(ESTADO_INACTIVO)));
 	}
 
 	@Test
-	public void buscarPorFecha() throws Exception { // arrange
-
-		// act - assert
-		mocMvc.perform(get("/mantenimientos/activos/2021-07-27").accept(MediaType.APPLICATION_JSON))
-				.andExpect(status().isOk());
+	public void buscarPorFecha() throws Exception {
+		mocMvc.perform(
+				get("/mantenimientos/activos/{fecha}", LocalDate.now().toString()).accept(MediaType.APPLICATION_JSON))
+				.andExpect(status().isOk()).andExpect(jsonPath("$", hasSize(1)));
 	}
 
 }
